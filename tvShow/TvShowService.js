e6dfc64eef2b8f237574151fc8e7c3d7f2fc1cd9
Emilia Botnen <emibvdb@gmail.com>
2018-10-16 21:04:59 +0200

    Update tv files

--- tvShow/TvShowService.js
+++ tvShow/TvShowService.js
@@ -1,11 +1,40 @@
 'use strict';
-
-class TvShowService {
+const TvShow = require('./TvShow');
+const createId = require('../utils/idUtil');
+ class TvShowService {
   constructor() {
-    this.tvShows = [];
+    this.tvShows = [
+      new TvShow(createId(), 'Mr.Robot', 'Drama'),
+      new TvShow(createId(), 'Black Mirror', 'Drama')
+    ];
   }
-  getAll() {
+   getAll() {
     return this.tvShows;
   }
+   getById(id) {
+    const tvShow = this.tvShows.find(tvShow => tvShow.id == id);
+    return tvShow;
+  }
+   createTvShow(name, genre) {
+    const id = createId();
+    const tvShow = new TvShow(id, name, genre);
+    this.tvShows.push(tvShow);
+    return tvShow;
+  }
+   remove(id) {
+    const tvShowToDelete = this.getById(id);
+    this.tvShows = this.tvShows.filter(tvShow => tvShow.id !== id);
+    return tvShowToDelete;
+  }
+   update(id, updatedTvShow) {
+    this.tvShows = this.tvShows
+      .map(tvShow => {
+        if (tvShow.id === id) {
+          return Object.assign({}, tvShow, updatedTvShow);
+        }
+        return tvShow;
+      });
+    return this.getById(id);
+  }
 }
-module.exports = new TvShowService();
+ module.exports = new TvShowService();
--- tvShow/tvShowRouter.js
+++ tvShow/tvShowRouter.js
@@ -1,20 +1,58 @@
 'use strict';
-
 const express = require('express');
-//const TvShow = require('./TvShow');
-const TvShowService = require('./TvShowService');
-//
+const tvShowService = require('./tvShowService');
+const reviewService = require('../review/reviewService');
 const tvShowRouter = express.Router();
-
-
-//GET http://localhost:3000/tvshow/
+ // GET http://localhost:3000/tvshow/
 tvShowRouter.get('/', (req, res) => {
-  //res.json(tvShows);
-  res.json(tvShowsService.getAll());
+  const tvShows = tvShowService.getAll().map(tvShow =>
+    Object.assign(
+      {},
+      tvShow,
+      {
+        reviews: `http://localhost:3000/tvShow/${tvShow.id}/review`
+      }
+    )
+  );
+  res.send(tvShows);
 });
-
-//GET http://localhost:3000/tvshow/1
-tvShowRouter.get('/{id}', (req, res) => {
-  res.json(tvShowsService.getAll().getElementById('id'));
+ // POST http://localhost:3000/tvshow/ Body: { "name": {name}, "genre": {genre} }
+tvShowRouter.post('/', (req, res) => {
+  const name = req.body.name;
+  const genre = req.body.genre;
+  const newTvShow = tvShowService.createTvShow(name, genre);
+  res.send(newTvShow);
 });
-
+ tvShowRouter.route('/:tvShowId')
+  // GET http://localhost:3000/tvshow/:tvShowId/
+  .get((req, res) => {
+    const tvShowId = req.params.tvShowId;
+    const tvShow = tvShowService.getById(tvShowId);
+    res.send(Object.assign(
+      {},
+      tvShow,
+      {
+        reviews: `http://localhost:3000/tvShow/${tvShow.id}/review`
+      }
+    ));
+  })
+  // PUT // http://localhost:3000/tvshow/:tvShowId/ Body: { "name": {name} }
+  .put((req, res) => {
+    const tvShowId = req.params.tvShowId;
+    const tvShowToUpdate = req.body;
+    const updatedTvShow = tvShowService.update(tvShowId, tvShowToUpdate);
+    res.send(updatedTvShow);
+  })
+  // DELETE http://localhost:3000/tvshow/:tvShowId/
+  .delete((req, res) => {
+    const tvShowId = req.params.tvShowId;
+    const deletedTvShow = tvShowService.remove(tvShowId);
+    res.send(deletedTvShow);
+  });
+ tvShowRouter.route('/:tvShowId/review')
+  .get((req, res) => {
+    const relatedItemId = req.params.tvShowId;
+    const tvShowReviews = reviewService.getAllForRelatedItem(relatedItemId);
+    res.send(tvShowReviews);
+  });
+ module.exports = tvShowRouter;
